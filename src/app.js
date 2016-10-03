646d736e1e11e23824c718909b4c13bb94085cd3
wlua <luawilson@hotmail.fr>
2016-10-03 11:45:07 +0200

    modif package, fonctionne

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ http.createServer(function(req, res){
   user.get("Wilson", function(id){
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    res.end('Hello' + id);
+    res.end('Hello ' + id);
   });
 
-}).listen(1555, '127.0.0.1');
+}).listen(1338, '127.0.0.1');
--- src/user.js
+++ src/user.js
@@ -1,7 +1,8 @@
 module.exports = {
-  name:"test",
-  lname: "test2",
-  getName(){
-    return this.name;
+  get: function(user, callback){
+    return callback(user);
+  },
+  save: function(id, callback){
+    return callback(id);
   }
 }
